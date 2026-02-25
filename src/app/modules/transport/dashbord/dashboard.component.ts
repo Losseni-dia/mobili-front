ef6f4c12653daeed248a764863d2ee72ae7bcfad
Losseni Dia <losdiakite@gmail.com>
2026-02-25 21:58:52 +0100

    dashboard component

---
+++ src/app/modules/transport/dashbord/dashboard.component.ts
@@ -0,0 +1,50 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CompanyService } from '../company/service/company.service';
+import { VehicleService } from '../vehicule/service/vehicle.service';
+import { Company } from '../company/model/company.model';
+import { Vehicle } from '../vehicule/model/vehicle.model';
+
+@Component({
+  selector: 'app-transport-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './transport-dashboard.component.html',
+  styleUrls: ['./transport-dashboard.component.scss']
+})
+export class TransportDashboardComponent implements OnInit {
+  private companyService = inject(CompanyService);
+  private vehicleService = inject(VehicleService);
+
+  companies: Company[] = [];
+  selectedVehicles: Vehicle[] = [];
+  selectedCompanyId: number | null = null;
+  loading: boolean = false;
+
+  ngOnInit() {
+    this.loadCompanies();
+  }
+
+  loadCompanies() {
+    this.loading = true;
+    this.companyService.getAll().subscribe({
+      next: (data) => {
+        this.companies = data;
+        this.loading = false;
+      },
+      error: () => this.loading = false
+    });
+  }
+
+  selectCompany(id: number) {
+    this.selectedCompanyId = id;
+    this.loading = true;
+    this.vehicleService.getByCompany(id).subscribe({
+      next: (data) => {
+        this.selectedVehicles = data;
+        this.loading = false;
+      },
+      error: () => this.loading = false
+    });
+  }
+}
