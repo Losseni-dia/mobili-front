c220c5b48a0ca8d7220bffac7f9bdcd299d6f6f6
Losseni Dia <losdiakite@gmail.com>
2026-02-25 18:58:41 +0100

    trip : component

---
+++ src/app/modules/trip/component/trip.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TripService } from './trip.service';
+import { Trip } from './trip.model';
+
+@Component({
+  selector: 'app-trip',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './trip.component.html',
+  styleUrls: ['./trip.component.scss']
+})
+export class TripComponent {
+  private tripService = inject(TripService);
+
+  departure: string = '';
+  arrival: string = '';
+  results: Trip[] = [];
+  loading: boolean = false;
+
+  find() {
+    if (!this.departure || !this.arrival) return;
+    this.loading = true;
+    this.tripService.search(this.departure, this.arrival).subscribe({
+      next: (data) => {
+        this.results = data;
+        this.loading = false;
+      },
+      error: () => this.loading = false
+    });
+  }
+}
