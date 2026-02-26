b39e27edcbd9592758263a22f100e63e06ca7ecb
Losseni Dia <losdiakite@gmail.com>
2026-02-26 01:02:23 +0100

    refactor

--- src/app/modules/transport/dashbord/dashboard.component.ts
+++ src/app/modules/transport/dashbord/dashboard.component.ts
@@ -1,9 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ChangeDetectorRef, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompanyService } from '../company/service/company.service';
 import { VehicleService } from '../vehicule/service/vehicle.service';
-import { Company } from '../company/model/company.model';
-import { Vehicle } from '../vehicule/model/vehicle.model';
 
 @Component({
   selector: 'app-transport-dashboard',
@@ -15,36 +13,39 @@ import { Vehicle } from '../vehicule/model/vehicle.model';
 export class TransportDashboardComponent implements OnInit {
   private companyService = inject(CompanyService);
   private vehicleService = inject(VehicleService);
+  private cdr = inject(ChangeDetectorRef);
+  private zone = inject(NgZone); 
 
-  companies: Company[] = [];
-  selectedVehicles: Vehicle[] = [];
+  companies: any[] = [];
+  selectedVehicles: any[] = [];
   selectedCompanyId: number | null = null;
-  loading: boolean = false;
 
   ngOnInit() {
     this.loadCompanies();
   }
 
   loadCompanies() {
-    this.loading = true;
     this.companyService.getAll().subscribe({
       next: (data) => {
-        this.companies = data;
-        this.loading = false;
+        this.zone.run(() => {
+          this.companies = data;
+          this.cdr.detectChanges();
+        });
       },
-      error: () => this.loading = false
+      error: (err) => console.error('Erreur chargement compagnies', err)
     });
   }
 
   selectCompany(id: number) {
     this.selectedCompanyId = id;
-    this.loading = true;
     this.vehicleService.getByCompany(id).subscribe({
       next: (data) => {
-        this.selectedVehicles = data;
-        this.loading = false;
+        this.zone.run(() => {
+          this.selectedVehicles = data;
+          this.cdr.detectChanges();
+        });
       },
-      error: () => this.loading = false
+      error: (err) => console.error('Erreur chargement véhicules', err)
     });
   }
 }
